149ce957484e97e0902ad9316d299b9ea85a20ae
Doğukan Ürker <dogukanurker@icloud.com>
2026-02-07 01:25:04 +0300

    refactor: centralize admin route authorization

--- app/routes/admin_panel_comments.py
+++ app/routes/admin_panel_comments.py
@@ -1,54 +1,34 @@
 from flask import (
     Blueprint,
-    redirect,
     render_template,
-    request,
     session,
 )
 
-from models import Comment, User
+from models import Comment
 from utils.log import Log
 from utils.paginate import paginate_query
+from utils.route_guards import admin_required
 
 admin_panel_comments_blueprint = Blueprint("admin_panel_comments", __name__)
 
 
 @admin_panel_comments_blueprint.route("/admin/comments", methods=["GET", "POST"])
+@admin_required("comment admin panel")
 def admin_panel_comments():
-    if "username" in session:
-        user = User.query.filter_by(username=session["username"]).first()
-        if not user:
-            return redirect("/")
+    Log.info(f"Admin: {session['username']} reached to comments admin panel")
 
-        if user.role != "admin":
-            Log.error(
-                f"{request.remote_addr} tried to reach comment admin panel without being admin"
-            )
-            return redirect("/")
+    query = Comment.query.order_by(Comment.time_stamp.desc())
+    comments_objects, page, total_pages = paginate_query(query)
 
-        Log.info(f"Admin: {session['username']} reached to comments admin panel")
+    comments = [
+        (c.id, c.post_id, c.comment, c.username, c.time_stamp) for c in comments_objects
+    ]
 
-        query = Comment.query.order_by(Comment.time_stamp.desc())
-        comments_objects, page, total_pages = paginate_query(query)
+    Log.info(f"Rendering admin_panel_comments.html: params: comments={len(comments)}")
 
-        comments = [
-            (c.id, c.post_id, c.comment, c.username, c.time_stamp)
-            for c in comments_objects
-        ]
-
-        Log.info(
-            f"Rendering admin_panel_comments.html: params: comments={len(comments)}"
-        )
-
-        return render_template(
-            "admin_panel_comments.html",
-            comments=comments,
-            page=page,
-            total_pages=total_pages,
-        )
-    else:
-        Log.error(
-            f"{request.remote_addr} tried to reach comment admin panel without being logged in"
-        )
-
-        return redirect("/")
+    return render_template(
+        "admin_panel_comments.html",
+        comments=comments,
+        page=page,
+        total_pages=total_pages,
+    )
